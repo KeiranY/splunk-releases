7c292e51ac96c3cbf443f6a358c1df7537c6e1b0
Keiran Young <KcYoung1997@hotmail.co.uk>
2021-01-08 16:49:05 +0000

    feat(cli): add hash checking
    MD5/SHA512 hashes can now be verified with the -c/--checksum parameter
    
    Axios has been replaced with Node https in cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,15 @@
-import http from 'http';
+import https from 'https';
 import fs from 'fs';
 import inquirer from 'inquirer';
 import { ListQuestion } from 'inquirer';
 import { Download, getDownloads } from './download';
 import { program } from 'commander';
 import chalk from 'chalk';
-import axios, { AxiosResponse } from 'axios';
 import cliProgress from 'cli-progress';
+import digestStream from 'digest-stream';
+import { pipeline } from 'stream';
+import crypto from 'crypto';
+import e from 'express';
 
 program
   .storeOptionsAsProperties(false)
@@ -20,6 +23,7 @@ program
     'filter to specified platform (enterprise/forwarder)',
     process.env.SPLUNKRELEASES_PRODUCT,
   )
+  .option('-c --checksum <md5|sha512>', 'calculate checksum of download')
   .parse(process.argv);
 
 const filter = async (downloads: Download[], question: string, field: string): Promise<Download[]> => {
@@ -50,35 +54,68 @@ export const main = async (): Promise<void> => {
   downloads = await filter(downloads, 'Choose a product', 'product');
   if (program.opts()['download']) {
     if (typeof program.opts()['download'] === 'string') {
-      download(downloads[0].link, program.opts()['download']);
+      download(downloads[0], program.opts()['download']);
     } else {
-      download(downloads[0].link, downloads[0].filename);
+      download(downloads[0], downloads[0].filename);
     }
   } else {
     console.log(chalk.bold('Link: ') + chalk.underline(downloads[0].link));
   }
 };
 
-const download = (link: string, filename: string) => {
-  const outFile = fs.createWriteStream(filename);
+const createHash = (): crypto.Hash | null => {
+  if (!program.opts()['checksum']) return null;
+  const hashes = ['md5', 'sha512'].filter((x) => crypto.getHashes().includes(x));
+  if (!hashes.includes(program.opts()['checksum'])) {
+    console.log(`${chalk.stderr('error:')} provided checksum type ${program.opts()['checksum']} is unsupported.
+      splunk-releases supports md5,sha512.
+      openSSL on this machine supports ${hashes}`);
+    process.exit(2);
+  }
+  return crypto.createHash(program.opts()['checksum']);
+};
+
+const checkHash = (download: Download, hash: crypto.Hash | null) => {
+  if (!hash) return;
+  https.get(program.opts()['checksum'] === 'md5' ? download.md5 : download.sha512, (res) => {
+    const body = [];
+    res.on('data', (c) => body.push(c));
+    res.on('end', () => {
+      const splunkHash = Buffer.concat(body).toString().split('=')[1].trim();
+      const downloadHash = hash.digest('hex');
+      if (splunkHash !== downloadHash) {
+        console.log(`${chalk.stderr('error:')} download ${program.opts()['checksum']} hash is ${chalk.stderr(downloadHash)} expected ${splunkHash}`);
+        process.exit(3);
+      } else {
+        console.log(`${program.opts()['checksum']} hash ${chalk.green(downloadHash)} matches`);
+      }
+    });
+  });
+};
+
+const download = (download: Download, filename: string) => {
+  const outFile = fs.createWriteStream(filename, { encoding: null });
   const bar = new cliProgress.SingleBar({
     format: `${chalk.green(filename)} ${chalk.cyan('{bar}')} | {percentage}% | {value}/{total}  | ETA: {eta}s`,
     barCompleteChar: '\u2588',
     barIncompleteChar: '\u2591',
     hideCursor: true,
   });
-  axios({
-    url: link,
-    method: 'GET',
-    responseType: 'stream',
-  }).then((result: AxiosResponse<http.ServerResponse>) => {
-    bar.start(result.headers['content-length'], 0);
-    result.data.on('data', (chunk) => bar.increment(chunk.length));
-    result.data.on('close', () => {
-      bar.stop();
-      console.log(chalk.bold('Downloaded to: ') + chalk.underline(outFile.path));
-    });
-    result.data.pipe(outFile);
+  https.get(download.link, (response) => {
+    bar.start(parseInt(response.headers['content-length']), 0);
+    const hash = createHash();
+    response
+      .on('close', () => {
+        bar.stop();
+        console.log(chalk.bold('Downloaded to: ') + chalk.underline(outFile.path));
+        outFile.close();
+        checkHash(download, hash);
+      })
+      .on('data', (chunk) => {
+        bar.increment(chunk.length);
+        if (hash) hash.update(chunk);
+      })
+      .pipe(outFile);
   });
 };
 

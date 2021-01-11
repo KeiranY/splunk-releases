4b7386e2262a18a450e24730e9b4ab629ee95a19
Keiran Young <KcYoung1997@hotmail.co.uk>
2021-01-11 05:22:23 +0000

    fix(cli): usable checksum flag

--- src/cli.ts
+++ src/cli.ts
@@ -127,11 +127,11 @@ program
   .action(details);
 
 program
+  .option('-c, --checksum <md5|sha512>', 'calculate checksum of download')
   .command('download [filename]')
   .aliases(['d', 'dl'])
   .description('download a splunk release', { checksum: 'calculate checksum of download' })
   .usage('download [filename] <c md5|sha512>')
-  .option('-c --checksum <md5|sha512>', 'calculate checksum of download')
   .action(download);
 
 if (require.main === module) {
